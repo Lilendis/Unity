6a7c5f58898c6d1b4e0364a30f1e7c0fc1ae807f
Lilendis <petrrogacev1@gmail.com>
2024-08-17 15:02:30 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Вывод Hello World 1 раз на экран
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
